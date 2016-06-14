82b5168807fe6d145d945f56bab750c142fb9461
nakedsushi <louise.m.yang@gmail.com>
2016-06-14 14:20:58 -0700

    Answers that were found in the DB are displayed as rich messages.

--- webhook/handler.js
+++ webhook/handler.js
@@ -21,7 +21,6 @@ AWS.config.update({
 const db  = new AWS.DynamoDB();
 module.exports.handler = function(event, context, callback) {
   if (event.method === 'GET') {
-    // facebook app verification
     if (event.hubVerifyToken === process.env.HUB_VERIFY_TOKEN && event.hubChallenge) {
       return callback(null, parseInt(event.hubChallenge));
     } else {
@@ -78,15 +77,31 @@ let replyAboutFood = (data, senderId) => {
   if (data.Item) {
     let item = JSON.parse(unmarshalJson(data.Item));
     let answer = 'No!';
-    if (item.answer) { answer = 'Yes, don’t worry.'; }
-    message = `${answer} ${item.body}`;
+    if (item.answer) { answer = 'Yes'; }
+    message = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [{
+            title: answer,
+            subtitle: item.body
+          }]
+        }
+      }
+    };
+    sendMessageToFacebook({
+      recipient: {
+        id: senderId
+      },
+      message: message
+    });
+  } else {
+    sendMessageToFacebook({
+      recipient: {
+        id: senderId
+      },
+      message: { text: message }
+    });
   }
-  sendMessageToFacebook({
-    recipient: {
-      id: senderId
-    },
-    message: {
-      text: message
-    }
-  });
 };
